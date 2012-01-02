220c3d233234fadf3623ae5f7ec99042f2358386
gedi <gediminas.morkevicius@gmail.com>
2012-01-02 22:49:36 +0200

    configurations, and data migration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,7 +9,7 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            //new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
@@ -29,7 +29,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            //$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
--- src/Gedmo/BlogBundle/Command/MigrateCommand.php
+++ src/Gedmo/BlogBundle/Command/MigrateCommand.php
@@ -13,6 +13,7 @@ use Gedmo\BlogBundle\Entity\EmailMessage;
 class MigrateCommand extends DoctrineCommand
 {
     private $em;
+    private $old;
 
     protected function configure()
     {
@@ -25,6 +26,11 @@ class MigrateCommand extends DoctrineCommand
                     'em', null, InputOption::VALUE_OPTIONAL,
                     'Used entity manager.',
                     'default'
+                ),
+                new InputOption(
+                    'old', null, InputOption::VALUE_OPTIONAL,
+                    'Old database name.',
+                    'blog'
                 )
             ))
         ;
@@ -34,6 +40,11 @@ class MigrateCommand extends DoctrineCommand
     {
         $emName = $input->getOption('em');
         $this->em = $this->getEntityManager($emName);
+        $this->old = \Doctrine\DBAL\DriverManager::getConnection(
+            array_merge($this->em->getConnection()->getParams(), array('dbname' => $input->getOption('old'))),
+            null,
+            $this->em->getEventManager()
+        );
 
         $this->process($output);
         $output->writeLn('Done');
@@ -41,6 +52,38 @@ class MigrateCommand extends DoctrineCommand
 
     private function process(OutputInterface $output)
     {
-        //
+        $new = $this->em->getConnection();
+        $new->beginTransaction();
+        try {
+            // article map
+            $map = array();
+            foreach ($this->old->fetchAll('select * from articles') as $o) {
+                $n = $new->fetchAssoc('select * from articles where slug = ? limit 1', array($o['slug']));
+                if ($n) {
+                    $map[intval($o['id'])] = intval($n['id']);
+                }
+            }
+            // update article creation dates and comments
+            foreach ($map as $oId => $nId) {
+                $o = $this->old->fetchAssoc('select * from articles where id = ? limit 1', array($oId));
+                $new->executeQuery('update articles set created = ? where id = ?', array($o['created'], $nId));
+
+                $cs = $this->old->fetchAll('select * from comments where article_id = ?', array($oId));
+                foreach ($cs as $c) {
+                    $nc = array(
+                        'created' => $c['created'],
+                        'article_id' => $nId,
+                        'author' => $c['author'],
+                        'subject' => $c['subject'],
+                        'content' => $c['content'] // ? into markdown?
+                    );
+                    $new->insert('comments', $nc);
+                }
+            }
+            $new->commit();
+        } catch(\Exception $e) {
+            $new->rollback();
+            throw $e;
+        }
     }
 }
